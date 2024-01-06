7134027b7a8984f22c71c407905ad1292983cfd5
gitperalta <sebssjb@gmail.com>
2024-01-05 20:18:05 -0500

    Feat: Configure ESLint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  // settings: {
+  //   react: {
+  //     version: "detect",
+  //   },
+  // },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "@typescript-eslint/space-before-function-paren": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
+};
