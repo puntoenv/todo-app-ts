c124212e0da8c27374964167a36ec5bec443f398
gitperalta <sebssjb@gmail.com>
2024-01-13 12:57:15 -0500

    fix: Some bug in the Todo and Todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,18 +1,30 @@
-import { type TodoCompleted, type TodoId, type Todo as TodoType } from '../types'
+import {
+  type TodoCompleted,
+  type TodoId,
+  type Todo as TodoType
+} from '../types'
 
 interface Props extends TodoType {
   onRemoveTodo: () => void
   onToggleCompleteTodo: ({ id, completed }: TodoId & TodoCompleted) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+export const Todo: React.FC<Props> = ({
+  id,
+  title,
+  completed,
+  onRemoveTodo,
+  onToggleCompleteTodo
+}) => {
   return (
     <div>
       <input
         type="checkbox"
         className="toggle"
         checked={completed}
-        onChange={(e) => { onToggleCompleteTodo({ id, completed: e.target.checked }) }}
+        onChange={(e) => {
+          onToggleCompleteTodo({ id, completed: e.target.checked })
+        }}
       />
       <label>{title}</label>
       <button className="destroy" onClick={onRemoveTodo}></button>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -7,20 +7,27 @@ interface Props {
   onToggleCompleteTodo: ({ id, completed }: TodoId & TodoCompleted) => void
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleteTodo }) => {
+export const Todos: React.FC<Props> = ({
+  todos,
+  onRemoveTodo,
+  onToggleCompleteTodo
+}) => {
   return (
-        <ul className='todo-list'>
-        {todos.map((todo) => (
-            <li key={todo.id} className={todo.completed ? 'completed' : ''}>
-                <Todo
-                key={todo.id}
-                id={todo.id}
-                title={todo.title}
-                completed={todo.completed}
-                onRemoveTodo={() => { onRemoveTodo({ id: todo.id }) } }
-                onToggleCompleteTodo={onToggleCompleteTodo}/>
-            </li>
-        ))}
-        </ul>
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            onRemoveTodo={() => {
+              onRemoveTodo({ id: todo.id })
+            }}
+            onToggleCompleteTodo={onToggleCompleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
   )
 }
